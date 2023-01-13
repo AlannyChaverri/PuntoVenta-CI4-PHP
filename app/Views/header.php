b19b8deae3bc3469704009a84e2717e3fc27b0fc
Alanny <achaverrim@est.utn.ac.cr>
2023-01-13 12:51:33 -0600

    cambios en el header cerrarsesion

--- app/Views/header.php
+++ app/Views/header.php
@@ -1,3 +1,7 @@
+<?php
+// para mostrar el nombre en la sesion
+$user_session = session();
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -19,7 +23,7 @@
 <body class="sb-nav-fixed">
     <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
         <!-- Navbar Brand-->
-        <a class="navbar-brand ps-3" href="<?php echo base_url(); ?>/">Tienda <i class="fa-solid fa-shop"></i></a>
+        <a class="navbar-brand ps-3" href="<?php echo base_url(); ?>/index">Tienda <i class="fa-solid fa-shop"></i></a>
         <!-- Sidebar Toggle-->
         <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
         <!-- Navbar Search-->
@@ -32,14 +36,14 @@
         <!-- Navbar-->
         <ul class="navbar-nav ms-auto me-0 me-md-3 my-2 my-md-0">
             <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
+                <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"> <?php echo $user_session->nombre; ?> <i class="fas fa-user fa-fw"></i></a>
                 <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                    <li><a class="dropdown-item" href="#!">Settings</a></li>
+                    <li><a class="dropdown-item" href="<?php echo base_url(); ?>/usuarios/cambia_password">Cambiar credenciales</a></li>
                     <li><a class="dropdown-item" href="#!">Activity Log</a></li>
                     <li>
                         <hr class="dropdown-divider" />
                     </li>
-                    <li><a class="dropdown-item" href="#!">Logout</a></li>
+                    <li><a class="dropdown-item" href="<?php echo base_url(); ?>/usuarios/logout">Cerrar sesion</a></li>
                 </ul>
             </li>
         </ul>
@@ -97,8 +101,8 @@
                             <nav class="sb-sidenav-menu-nested nav">
                                 <a class="nav-link" href="<?php echo base_url(); ?>/configuracion">Configuracion</a>
                                 <a class="nav-link" href="<?php echo base_url(); ?>/cajas">Cajas</a>
-                                <a class="nav-link" href="<?php echo base_url(); ?>/usuarios">Usuarios</a>
                                 <a class="nav-link" href="<?php echo base_url(); ?>/roles">Roles</a>
+                                <a class="nav-link" href="<?php echo base_url(); ?>/usuarios">Usuarios</a>
                             </nav>
                         </div>
 
